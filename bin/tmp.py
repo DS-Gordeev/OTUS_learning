20348eee20bdb9efe2323b31ea572cf60d1a0df4
DS-Gordeev <gordeyka@list.ru>
2023-06-26 23:01:56 +0300

    someone commit

--- bin/tmp.py
+++ bin/tmp.py
@@ -2,7 +2,7 @@ import socket
 from http import HTTPStatus
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    srv_addr = ('172.20.160.1', 5000)
+    srv_addr = ('192.168.1.3', 5000)
     s.bind(srv_addr)
     s.listen(1)
 
@@ -20,24 +20,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         text = recv_bytes.decode('utf-8')
         print(f'Message received: {text}')
 
-        status_line = 'HTTP/1.1 200 OK'
+        # Получаем метод  --> GET
         req_method = text.split('\n')[0].split(' ')[0]
-        status = text.split('\n')[0].split(' ')[1]
-        headers_list = text.split('\n')[1:]
-        print(type(headers_list[-1]))
-        heads = '\n'.join((lambda x: [f'<p>Header-name: {i}</p>' for i in x if i != ''])(headers_list))
 
+        # Получаем статус  --> / или /?status=404
+        status = text.split('\n')[0].split(' ')[1]
+
+        # Получаем список всех заголовков (берем все кроме первой)  -->
+        # ['Host: 192.168.1.3:5000', 'Connection: keep-alive', ...]
+        headers_list = text.split('\n')[1:]
+
+        # При помощи лямбда-функции формируем список строк-заголовков и джойним их в одну строку
+        heads_to_body = '\n'.join((lambda x: [f'<p>Header-name: {i}</p>' for i in x if ':' in i])(headers_list))
+
+        # Создаем объект httpStatus с кодом из параметра запроса /?status=404  --> 404
         try:
             status_code = HTTPStatus(int(status.split('=')[1]))
+        # В случае если статус ошибочный (ValueError) или endpoint без параметра статуса (IndexError) - присваиваем 200
         except (ValueError, IndexError):
             status_code = HTTPStatus(200)
 
         body = '<h1>Hello from OTUS!</h1>' \
-        f'<p>Request Method: {req_method}</p>' \
-        f'<p>Response Status: {status_code.value, status_code.name}</p>' \
-        f'<p>Request Source: {addr}</p>' \
-        f'{heads}'
+               f'<p>Request Method: {req_method}</p>' \
+               f'<p>Response Status: {status_code.value, status_code.name}</p>' \
+               f'<p>Request Source: {addr}</p>' \
+               f'{heads_to_body}'
 
+        status_line = 'HTTP/1.1 200 OK'
         headers = '\r\n'.join([status_line, 'Content-Type: text/html', f'Content-Lenght: {len(body)}'])
         resp = '\r\n\r\n'.join([headers, body])
         sent_bytes = conn.send(resp.encode('utf-8'))
